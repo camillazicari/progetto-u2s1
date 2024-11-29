86aaddec7c14e4602fc0fc388b5b1457b8de47f4
camillazicari <camyzicari01@gmail.com>
2024-11-29 16:59:53 +0100

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector('nav');
+const navBtn = document.querySelector('nav ul li button');
 const header = document.querySelector('header')
 
 const navHeight = 380
@@ -6,7 +7,9 @@ const navHeight = 380
 window.addEventListener('scroll', function() {
     if (window.scrollY >= navHeight) {
         nav.classList.add('changecolor');
+        navBtn.classList.add('colorChanger');
     } else  {
         nav.classList.remove('changecolor');
+        navBtn.classList.remove('colorChanger');
     }
 })
