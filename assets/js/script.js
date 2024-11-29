eda6cc2031689b693b0127891a8f1eedf6bf08b4
camillazicari <camyzicari01@gmail.com>
2024-11-29 18:26:46 +0100

    random M

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,8 @@
 const nav = document.querySelector('nav');
 const navBtn = document.querySelector('nav ul li button');
 const header = document.querySelector('header')
-
 const navHeight = 370;
+
 
 window.addEventListener('scroll', function() {
     if (window.scrollY >= navHeight) {
@@ -13,3 +13,31 @@ window.addEventListener('scroll', function() {
         navBtn.classList.remove('colorChanger');
     }
 })
+
+function randomLetters() {
+    const gs = document.querySelectorAll('g[stroke-linecap=butt]');
+    let random = Math.floor(Math.random()*gs.length);
+    let currentM = gs[random];
+
+    return currentM;
+}
+
+function changeOpacity() {
+    let m = randomLetters();
+    let opacity = parseInt(m.getAttribute('opacity'));
+    if (opacity === 0) {
+        m.setAttribute('opacity', 1);
+    } else {
+        m.setAttribute('opacity', 0);
+    }
+}
+
+function invisibleVisibleM() {
+    setInterval(function() {
+        changeOpacity()
+    }, 45);
+}
+
+window.onload = function() {
+    invisibleVisibleM();
+}
